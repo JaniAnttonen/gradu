35ad148366a5d06926b3f26588e8bc80894b6c22
Jani Anttonen <jantto@utu.fi>
2020-05-25 19:36:14 +0300

    Also build png of the second diagram

--- build_diagrams.sh
+++ build_diagrams.sh
@@ -1,2 +1,3 @@
 mscgen -T eps -o pictures/pol1_diagram.eps -i protocol_diagram_1.mscgen
 mscgen -T eps -o pictures/pol2_diagram.eps -i protocol_diagram_2.mscgen
+mscgen -T png -o pictures/pol2_diagram.png -i protocol_diagram_2.png
