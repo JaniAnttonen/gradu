e6c8616489ab8fd098ad9029b286e8b75c1d0ed6
JaniAnttonen <jani.anttonen@protonmail.ch>
2022-04-08 16:44:39 +0300

    Update CI build scripts

--- build_diagrams.sh
+++ build_diagrams.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for filename in src/pictures/*.mscgen; do
-    mscgen -T eps -o "src/pictures/$(basename "$filename" .mscgen).eps" -i "$filename"
-    mscgen -T png -o "src/pictures/$(basename "$filename" .mscgen).png" -i "$filename"
+    ./mscgen-0.20/bin/mscgen -T eps -o "src/pictures/$(basename "$filename" .mscgen).eps" -i "$filename"
+    ./mscgen-0.20/bin/mscgen -T png -o "src/pictures/$(basename "$filename" .mscgen).png" -i "$filename"
 done
